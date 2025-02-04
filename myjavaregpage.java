f56d2bff614888c28437e2819978fc251bc29f41
Purushotham <lrp.purushotham@gmail.com>
2025-02-04 22:38:21 +0530

    dev task completed

--- myjavaregpage.java
+++ myjavaregpage.java
@@ -2,8 +2,18 @@ this is my java reg code
 1st line code added by dev a
 2nd line code added by dev b
 3rd line code added by dev a
+<<<<<<< Updated upstream
 4th line code added by dev b
 5th line code added by dev a
 
 
 the manager work completed
+=======
+
+1st day of java code completed
+2nd day of java code completed
+3rd day of java code completed
+4th day of java code completed
+5th day of java code completed
+
+>>>>>>> Stashed changes
